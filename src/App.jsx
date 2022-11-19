7eae6d91aa972a28abf776158095e2781d9defce
aravind-iorta <aravind.swamy@iorta.in>
2022-11-19 14:03:47 +0530

    base commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+import Product from "./components/Product";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Product />
+    </div>
+  );
+}
+
+export default App;
+
+// add product => img / name
+// add sub prodctt => img / desc / price
